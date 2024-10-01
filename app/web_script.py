6dd1ce2e72deb1935182da33ef4295ec2732e24f
FrankZhaoYX <zhaoyixi0306@gmail.com>
2024-10-01 16:56:51 -0400

    scrape the general info of each movie and start build the advanced mining method

--- app/web_script.py
+++ app/web_script.py
@@ -7,20 +7,31 @@ import numpy as np, pandas as pd
 import time
 
 names = []
+
 years = []
+lengths = []
+rating_years = []
+
 imdb_ratings = []
 metascores = []
 votes = []
+contents = []
+
 years_url = [str(i) for i in range(2000,2022)]
 pages = [str(i) for i in range(1,10)]
+
+
+
 # Preparing the monitoring of the loop
 
-genres=[]
 requests = 0
 runtimes=[]
 
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
+
+def metadata_scrape(url):
+    print(url)
 
 # For every year in the interval 2000-2022
 for year_url in years_url:
@@ -55,55 +66,73 @@ for year_url in years_url:
         # Parse the content of the request with BeautifulSoup
         page_html = BeautifulSoup(response.text, 'html.parser')
 
-        print("page_html is ")
-        print('https://www.imdb.com/search/title?release_date=' + year_url +
-        '&sort=num_votes,desc&page=' + page)
+        # print("page_html is ")
+        # print('https://www.imdb.com/search/title?release_date=' + year_url +
+        # '&sort=num_votes,desc&page=' + page)
 
         # Select all the 50 movie containers from a single page
-        mv_containers = page_html.find_all('div', class_ = 'lister-item mode-advanced')
-        print(mv_containers)
-        # For every movie of these 50
-        for container in mv_containers:
-            # If the movie has a Metascore, then:
-            if container.find('div', class_ = 'ratings-metascore') is not None:
+        mv_containers_1 = page_html.find_all('div', class_ = 'ipc-metadata-list-summary-item__tc')
+        mv_containers_2 = page_html.find_all('div', class_ = 'ipc-metadata-list-summary-item__c')
 
-                # Scrape the name
-                name = container.h3.a.text
-                print(name)
-                names.append(name)
+        # For every movie of these 50 top 25
+        for container in mv_containers_1:
+            # Scrape the name
+            name = container.find('h3', class_ = 'ipc-title__text').get_text()
+            names.append(name)
+            # print(name)
+            # Scrape the metadata
+            # metadata_set = container.find_all('span', class_ = 'sc-b189961a-8 hCbzGp dli-title-metadata-item')
+            metadata_set = container.find_all('span', class_ = "sc-ab348ad5-8 cSWcJI dli-title-metadata-item")
+            year = metadata_set[0].get_text()
+            years.append(year)
+            length = metadata_set[1].get_text()
+            lengths.append(length)
+            rating_year = metadata_set[2].get_text()
+            rating_years.append(rating_year)
 
-                # Scrape the year
-                year = container.h3.find('span', class_ = 'lister-item-year').text
-                print(year)
-                years.append(year)
+            # Scrape the IMDB_rating
+            imdb = float(container.find('span', class_= "ipc-rating-star--rating").get_text())
+            imdb_ratings.append(imdb)
 
-                # Scrape the IMDB rating
-                imdb = float(container.strong.text)
-                print(imdb)
-                imdb_ratings.append(imdb)
+            # Scrape the votes
+            vote_text = container.find('span', class_='ipc-rating-star--voteCount').get_text(strip=True)
+            vote = vote_text.strip("()")
+            votes.append(vote)
 
-                # Scrape the Metascore
-                m_score = container.find('span', class_ = 'metascore').text
-                print(m_score)
-                metascores.append(int(m_score))
+            # Scrape the Metasocre
+            m_scroe = int(container.find("span", class_= "sc-b0901df4-0 bXIOoL metacritic-score-box").get_text())
+            metascores.append(m_scroe)
 
-                # Scrape the number of votes
-                vote = container.find('span', attrs = {'name':'nv'})['data-value']
-                print(vote)
-                votes.append(int(vote))
-                
-            if container.p.find('span', class_ = 'genre') is not None:
-                genre = container.p.find('span', class_ = 'genre').text.replace("\n", "").rstrip().split(',') # remove the whitespace character, strip, and split to create an array of genres
-                genres.append(genre)
-            else:
-                genres.append("")
+            # Scrape the centent
+            content = container.find('div', class_= "ipc-html-content-inner-div").get_text()
+            contents.append(content)
+
+            # Scrape Each movie Metadata 
+            link = container.find('a', class_= "ipc-title-link-wrapper")['href']
+            link = "www.imdb.com" + link
+            metadata_scrape(link)
+
+names = []
+
+years = []
+lengths = []
+rating_years = []
+
+imdb_ratings = []
+metascores = []
+votes = []
+contents = []
+
+# 'genres': genres,
+
 
 movie_ratings = pd.DataFrame({'movie': names,
 'year': years,
+'length': lengths,
+'US certificates': rating_years,
 'imdb': imdb_ratings,
 'metascore': metascores,
 'votes': votes,
-'genres': genres,
 })
 print(movie_ratings.info())
 movie_ratings.head(10)
